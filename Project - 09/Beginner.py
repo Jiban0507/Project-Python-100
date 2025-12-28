516aa0ea2bea00abaa60280b3d2c5eee91d7f6f5
Debanga-06 <debanga078@gmail.com>
2025-12-28 18:31:06 +0530

    Sync project structure with local changes

--- Project - 09/Beginner.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def roll_dice():
-    return random.randint(1, 6)
-
-if __name__ == "__main__":
-    print("Rolling the dice...")
-    result = roll_dice()
-    print(f"You rolled a {result}!")
